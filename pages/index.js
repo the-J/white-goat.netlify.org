a773b31226a52c85d517a1887e223c6b39e2a458
Juliusz Jakubowski <the-j@github.noreply.com>
2020-12-13 18:14:18 +0100

    styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import {Container, GlobalStyle, Layout} from "./index.styles";
+import {Container, GlobalStyle, Layout} from "../components/index.styles";
 
 const Home = () => (
   <>
